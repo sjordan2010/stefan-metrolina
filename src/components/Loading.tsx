f4f18ee01ab347d5d6ec7382d2c8c3853dae70f9
Stefan Jordan <sjordan2010@gmail.com>
2023-05-25 01:11:32 -0400

    loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,5 @@
+import ReactLoading from "react-loading";
+
+export default function Loading() {
+  return <ReactLoading className="m-auto mt-48" type={"spinningBubbles"} color={"#03fc4e"} height={100} width={100} />;
+}
